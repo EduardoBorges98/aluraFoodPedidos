8974d350aa41d05a3fab18fb430b0acd04737b84
Eduardo Borges <eduardo_borges98@hotmail.com>
2026-05-04 21:42:12 -0300

    rabbitmq alt

--- src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,13 +1,35 @@
 package br.com.alurafood.pedidos.amqp;
 
-import org.springframework.amqp.core.Message;
+import br.com.alurafood.pedidos.dto.PagamentoDto;
+import br.com.alurafood.pedidos.dto.StatusDto;
+import br.com.alurafood.pedidos.model.Status;
+import br.com.alurafood.pedidos.service.PedidoService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PagamentoListener {
+
+    private final PedidoService pedidoService;
+
+    public PagamentoListener(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
     @RabbitListener(queues = "pagamento.concluido")
-    public void recebeMensagem(Message mensagem) {
-        System.out.println("Recebi a mensagem " + mensagem.toString());
+    public void recebeMensagem(PagamentoDto pagamento) {
+        System.out.println("Recebi pagamento: " + pagamento.getId());
+        System.out.println("Pedido ID: " + pagamento.getPedidoId());
+        System.out.println("Status do pagamento: " + pagamento.getStatus());
+
+        if (!"CONFIRMADO".equals(String.valueOf(pagamento.getStatus()))) {
+            System.out.println("Pagamento ainda não confirmado. Ignorando mensagem.");
+            return;
+        }
+
+        StatusDto statusDto = new StatusDto();
+        statusDto.setStatus(Status.PAGO);
+
+        pedidoService.atualizaStatus(pagamento.getPedidoId(), statusDto);
     }
 }
---
+++ src/main/java/br/com/alurafood/pedidos/dto/PagamentoDto.java
@@ -0,0 +1,24 @@
+package br.com.alurafood.pedidos.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagamentoDto {
+    private Long id;
+    private BigDecimal valor;
+    private String nome;
+    private String numero;
+    private String expiracao;
+    private String codigo;
+    private String status;
+    private Long formaDePagamentoId;
+    private Long pedidoId;
+}
